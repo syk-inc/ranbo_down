b0fc4ff4da73b5726c693b2b5571efb87a1b585e
akai1256 <happy2.cry2@gmail.com>
2018-10-14 17:45:55 +0900

    ナイフの画像を追加しました

--- core/src/akai1256/ranbodown/RanboDown.java
+++ core/src/akai1256/ranbodown/RanboDown.java
@@ -13,7 +13,7 @@ public class RanboDown extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("knife.png");
 	}
 
 	@Override
