56949b87f3dfb7e90658f5d30838def12a52f37e
akai1256 <happy2.cry2@gmail.com>
2018-10-14 17:10:39 +0900

    first commit

---
+++ desktop/src/akai1256/ranbodown/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package akai1256.ranbodown.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import akai1256.ranbodown.RanboDown;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RanboDown(), config);
+	}
+}
